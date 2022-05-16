d726639e94b6fb49ca76c6ac431f956f7576448f
leonhma <67312077+leonhma@users.noreply.github.com>
2022-05-16 20:36:52 +0200

    initial commit

---
+++ pages/api/putput-claim-coupon.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { sign } from 'jsonwebtoken'
+import { v4 as uuidv4 } from 'uuid'
+
+type Data = {
+  code: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const site_type = req.query.site_type || req.body.site_type || 'HDFILME.TV'
+  const duration_type = req.query.duration_type || req.body.duration_type || '1_day'
+    
+  const data = await fetch('https://api.putput.net/api/shared/claim-coupon/', {
+    'method': 'PUT',
+    'headers': {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    'body': JSON.stringify({
+      'couponToken': sign({
+        duration_type,
+        site_type,
+        jti: uuidv4()
+      }, 'hoan_an_cac')
+    })
+  })
+  res.status(200).json(await data.json())
+}
